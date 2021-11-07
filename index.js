283a7d413b14c75b9fead5fdd67fe7eac4e11263
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-11-07 21:31:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const http = require('http')
 const express = require('express')
+const favicon = require('serve-favicon')
 
 const app = express()
 const port = process.env.PORT || 7373
+const Main = require('./routes/main')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-    res.render('index')
-})
+app.use('/', Main)
+
 
 app.listen(port, () => {
     console.log(`Listening to http://localhost:${port} on port ` + port)
